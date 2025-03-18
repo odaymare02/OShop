f7c32dcac7a300211761b2d158abf3d963e94aa6
odaymare02 <mareoday123@gmail.com>
2025-03-19 01:47:05 +0200

    Add project files.

---
+++ OShop.API/Controllers/CategoriesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OShop.API.Data;
+using OShop.API.Models;
+
+namespace OShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        ApplicationDbContext _context;
+        public CategoriesController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+        [HttpGet("")]
+        public IActionResult GetAll()
+        {
+            var categories = _context.Categories.ToList();
+            return Ok(categories);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute]int id)
+        {
+            var Category = _context.Categories.Find(id);//do chashing when use find that mean when i request first time it get me after that make chase to this when request same thing it return from chash
+            return Category == null ? NotFound() : Ok(Category);
+        }
+
+        [HttpPost("")]
+        public IActionResult Create([FromBody]Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            //return Created($"{Request.Scheme}://{Request.Host}/");//201 mean success to add
+            return CreatedAtAction("GetById", new { Id = category.Id }, category);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute]int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null) return NotFound();
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return NoContent();//204
+        }
+    }
+}
---
+++ OShop.API/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace OShop.API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
